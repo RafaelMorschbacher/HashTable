5eaa7b666e0ca7d5789f549df3c4e3b2dbda0dad
RafaelMorschbacher <rafa2002cunha@gmail.com>
2023-02-26 20:31:06 -0300

    Busca versao 1
    Busca de nomes que estão funciona, busca de nomes que não estão não funciona

--- hash.py
+++ hash.py
@@ -13,34 +13,45 @@ def get_names(file):
 names = get_names(file_names)
 
 
-def hashFunction(input, M, i):
+def hashPolinomial(input, M, i):
     length = len(input)
     a = 31
     polinomio = 0
     for j in range(0, length):
-        polinomio = polinomio + ord(input[j]) * a**j
-    key = (polinomio + i)% M
+        polinomio = polinomio + ord(input[j]) * a ** j
+    key = (polinomio + i) % M
     return key
 
-def createHashTable(M, arr):
 
+def createHashTable(M, arr, hashFunction):
     hash_table = [None] * M
 
-    i = 0 #collision counter
+    i = 0  # collision counter
 
     for name in arr:
         key = hashFunction(name, M, i)
-        if hash_table[key] == None:
-            hash_table[key] = name
-        else:
-            while hash_table[key] != None:
-                i = i+1
-                key = hashFunction(name, M, i)
-            hash_table[key] = name
+
+        while hash_table[key] is not None:
+            i = i + 1
+            key = hashFunction(name, M, i)
+        hash_table[key] = name
     return hash_table
 
 
-table = createHashTable(10000, names)
-print(table)
-print(table.count(None))
+def searchName(name, hash_table, M, hashFunction):
+    key = hashFunction(name, M, 0)
 
+    if hash_table[key] is None:
+        return 'NÃO ENCONTRADO'
+
+    i = 0
+    while hash_table[key] != name and hash_table[key] != None:
+        i = i + 1
+        key = hashFunction(name, M, i)
+    return hash_table[key]
+
+
+table = createHashTable(10000, names, hashPolinomial)
+print(table)
+print(searchName("Mikenzie Symone", table, 10000, hashPolinomial))
+print(searchName("Rafael Cunha", table, 10000, hashPolinomial))
