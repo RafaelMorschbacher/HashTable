21e4a0d79863f072ad736123b11e57b7a8e6f2ae
RafaelMorschbacher <rafa2002cunha@gmail.com>
2023-02-26 19:46:00 -0300

    createTable versao 1
    Não está claro se funciona ou não, está preenchendo todos os elementos da tabela

--- hash.py
+++ hash.py
@@ -9,20 +9,38 @@ def get_names(file):
         names.append(name)
     return names
 
+
+names = get_names(file_names)
+
+
 def hashFunction(input, M, i):
     length = len(input)
     a = 31
     polinomio = 0
     for j in range(0, length):
         polinomio = polinomio + ord(input[j]) * a**j
-        print(ord(input[j]))
-    key = polinomio % M + i
+    key = (polinomio + i)% M
     return key
 
 def createHashTable(M, arr):
+
     hash_table = [None] * M
 
-# names = get_names(file_names)
-# for name in names:
-#     print(hashFunction(name, 10000, 0))
+    i = 0 #collision counter
+
+    for name in arr:
+        key = hashFunction(name, M, i)
+        if hash_table[key] == None:
+            hash_table[key] = name
+        else:
+            while hash_table[key] != None:
+                i = i+1
+                key = hashFunction(name, M, i)
+            hash_table[key] = name
+    return hash_table
+
+
+table = createHashTable(10000, names)
+print(table)
+print(table.count(None))
 
